c84f2fcfedca7d1bb45b84707878492767a7cefd
K1dou <hique1276@gmail.com>
2024-05-26 10:22:04 -0300

    first commit

---
+++ src/main/java/com/K1dou/Loja/virtual/LojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.K1dou.Loja.virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/K1dou/Loja/virtual/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.K1dou.Loja.virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
