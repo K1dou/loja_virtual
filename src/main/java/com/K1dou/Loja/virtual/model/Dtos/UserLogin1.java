8309f04cabf19d0817e1a04ac25a6414d4f7aae0
K1dou <hique1276@gmail.com>
2024-06-03 14:58:34 -0300

    commit

--- src/main/java/com/K1dou/Loja/virtual/model/Dtos/UserLogin1.java
+++
@@ -1,4 +0,0 @@
-package com.K1dou.Loja.virtual.model.Dtos;
-
-public record UserLogin1(String username,String password) {
-}
--- src/main/java/com/K1dou/Loja/virtual/model/Dtos/UserRegister1.java
+++
@@ -1,6 +0,0 @@
-package com.K1dou.Loja.virtual.model.Dtos;
-
-import com.K1dou.Loja.virtual.enums.Role;
-
-public record UserRegister1(String username, String password, Role role) {
-}
